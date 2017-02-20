e39cc00e88ef80902f0104dea80dcb60f739c309
Paulina Berger <paulina.berger@gmail.com>
2017-02-20 10:19:57 +0100

    second commit with new branch

--- app/src/main/java/org/mdsd2016/android/intents_filters_notifications/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/intents_filters_notifications/MainActivity.java
@@ -2,12 +2,38 @@ package org.mdsd2016.android.intents_filters_notifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnStandardNotification = (Button) findViewById(R.id.buttonToNotify);
+        btnStandardNotification.setOnClickListener(this);
+
+        final Button btnCustomNotification = (Button) findViewById(R.id.buttonToCustomNotify);
+        btnCustomNotification.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View whichView){
+
+        switch (whichView.getId()){
+            case R.id.buttonToCustomNotify:
+                Log.i(MainActivity.TAG, "CUstom notificaiton button clicked");
+                break;
+            case R.id.buttonToNotify:
+                Log.i(MainActivity.TAG, "Notification button clicked");
+                break;
+            default:
+                Log.i(MainActivity.TAG, "Clicked item not implemented");
+        }
     }
 }
