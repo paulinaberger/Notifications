0437621447f054ffaf5984681bb0dc4c68e46270
Paulina Berger <paulina.berger@gmail.com>
2017-02-20 10:49:03 +0100

    including Notification Activity

--- app/src/main/java/org/mdsd2016/android/intents_filters_notifications/MainActivity.java
+++ app/src/main/java/org/mdsd2016/android/intents_filters_notifications/MainActivity.java
@@ -1,7 +1,12 @@
 package org.mdsd2016.android.intents_filters_notifications;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
         switch (whichView.getId()){
             case R.id.buttonToCustomNotify:
+                triggerRegularNotitication();
                 Log.i(MainActivity.TAG, "CUstom notificaiton button clicked");
                 break;
             case R.id.buttonToNotify:
@@ -35,5 +41,23 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             default:
                 Log.i(MainActivity.TAG, "Clicked item not implemented");
         }
+    }
+
+    private void triggerRegularNotitication() {
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat().Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher).setContentTitle("Mynotification")
+                .setContentText("Thisisasimpletextformynotification");
+
+        Intent notificationIntent= new Intent(this, NotificationActivity.class);
+
+        PendingIntent mPendingIntent= PendingIntent.getActivity(this,1,notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        mBuilder.setContentIntent(mPendingIntent);
+
+        NotificationManager mNotifyManager = (NotificationManager) this
+                .getSystemService(Service.NOTIFICATION_SERVICE);
+
+        mNotifyManager.notify(2, mBuilder.build());
     }
 }
---
+++ app/src/main/java/org/mdsd2016/android/intents_filters_notifications/NotificationActivity.java
@@ -0,0 +1,8 @@
+package org.mdsd2016.android.intents_filters_notifications;
+
+/**
+ * Created by paulinaberger on 2017-02-20.
+ */
+
+public class NotificationActivity {
+}
